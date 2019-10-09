458f2b6ad7ce710cb0bbdc5bc8fe1e474dcc8702
Ben Koval <bethanykoval@gmail.com>
2019-10-09 15:46:52 -0400

    New navbar style and icon

--- src/components/Headers.vue
+++ src/components/Headers.vue
@@ -1,9 +1,9 @@
 <template>
-    <header>
+    <header class='header'>
         <nav class='navBar'>
             <ul>
-                <li><img class='logo' src='https://codefordurham.com/wp-content/themes/code-for-durham-website-master/images/logo/logo_2X.png' alt='Code for Durham logo'/></li>
-                <li class='navOptions'><a href='#lorem'>Lorem</a></li>
+                <li class='navOptions'><img class='logo' src='https://bean-project-screenshots.s3.amazonaws.com/c4d_logo-2.png' alt='Code for Durham logo'/></li>
+                <li class='navOptions'><a href='#lorem'>Code for Durham</a></li>
                 <li class='navOptions'><a href='#ipsum'>Ipsum</a></li>
                 <li class='navOptions'><a href='#dolor'>Dolor</a></li>
                 <li class='navOptions'><a href='#sit'>Sit</a></li>
@@ -12,3 +12,128 @@
         </nav>
     </header>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Playfair+Display:900&display=swap');
+/* $lato: 'Lato', sans-serif;
+$play: 'Playfair Display', serif; */
+
+.header {
+    padding-bottom: 20vh;
+}
+.logo {
+    height: 20vh;
+    border-radius: 50%;
+}
+
+.navBar {
+    /* background-image: linear-gradient(to right, #399aca, #5442f5); */
+    background-color: #399aca;
+    width: 100vw;
+    height: 12vh;
+    position: fixed;
+    z-index: 99;
+}
+/* * {
+    box-sizing: border-box;
+    margin: 0;
+}
+
+html {
+    scroll-behavior: smooth;
+}
+
+.navBar {
+    background-image: linear-gradient(to right, #399aca, #5442f5);
+    width: 100vw;
+    height: 10vh;
+    position: fixed;
+    z-index: 99;
+}
+a {
+    text-decoration: none;
+    color: inherit;
+}
+
+.logo {
+    height: 10vh;
+}
+
+ul {
+    display: flex;
+    justify-content: center;
+    width: inherit;
+    height: inherit;
+    list-style-type: none;
+}
+
+.navOptions {
+    margin: auto;
+    color: #fff;
+    font-size: 1rem;
+    border-bottom: .1rem solid #fff;
+    font-family: 'Lato', sans-serif;
+    text-transform: uppercase;
+}
+
+
+.mainContainer {
+    display: grid;
+    justify-content: center;
+}
+
+.textContainer {
+    margin: auto;
+}
+
+#lorem-title {
+    margin-top: 10%;
+}
+.title {
+    font-family: 'Playfair Display', serif;
+    font-size: 4rem;
+    color: #2f3159;
+    text-align: center;
+    padding: 10%;
+    width: 40vw;
+}
+
+.text {
+    color: #2b2b2b;
+    font-size: 1.5rem;
+    line-height: 175%;
+    padding: 5%;
+    font-family: 'Lato', sans-serif;
+    text-align: center;
+    width: 40vw;
+} */
+/* 
+// .halfContainer {
+//     display: flex;
+// }
+.mapImg {
+    // width: 40vw;
+    padding-top: 10%;
+    // padding-bottom: 5%;
+    height: 85vh;
+} */
+
+/* .intro,
+.outro {
+  padding: 20vh;
+} */
+
+/* // additions and overrides of DOM elements vue-scrollama sets up
+.scrollama-container {
+  display: grid;
+  grid-template-areas:
+    "graphic text";
+  
+  .scrollama-graphic {
+    grid-area: text;
+    height: 80vh;
+    top: 10vh;
+  }
+} */
+</style>
