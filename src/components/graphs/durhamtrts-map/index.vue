9ab1f305515dd9ccd9656bb99993d7f765f13468
Ben Koval <bethanykoval@gmail.com>
2019-10-13 23:36:16 -0400

    Add links to main navbar, change style of menu

--- src/components/graphs/durhamtrts-map/index.vue
+++ src/components/graphs/durhamtrts-map/index.vue
@@ -55,7 +55,7 @@ Data:
       <div class='step textContainer' id='home' data-step-no="hinc16">
           <h3 class='title'>Median Household Income in 2016</h3>
 
-          <p class='text'>Fixie dirty durham consensus big green wall beaver queen sustainable urban ministries dapper, broad street wool e bull old north innovation bowtie maker, southsquare historic preservation maker full frame sodu the buehler. The bulls hipster blues major sodu duke chapel dino trail eagles, major the state of things ninth street miami boulevard diy. Durham goldenbelt duke park local erwin road saint joseph's, start-up adf miami boulevard bulltown strutters ccb plaza french press, erwin terrace the double nickel merge records elf. Angier drive medicine dance the indy one forty seven train arm wrestling, duke chapel university science and math partner erwin square black wall street major, seventy french press dance dur'm 15-501. Blue devils bouncing bulldogs third fridays the double nickel chapel drive beer doughman seventy west village wunc, northeast central luewwd hipster hub coworking erwin square authentic innovation. Seeds the buehler oprah building walltown arm wrestling historic preservation local, blackwell urban ministries old north the kress historic preservation doughman, trinity park foodie bay 7 eno river bragtown.</p>
+          <p class='text'>Fixie dirty durham consensus big green wall beaver queen sustainable urban ministries dapper, broad street wool e bull old north innovation bowtie maker, southsquare historic preservation maker full frame sodu the buehler.</p>
       </div>
       <div class='step textContainer' id='' data-step-no="hincw16">
           <h3 class='title'>Median Household Income for White Residents in 2016</h3>
