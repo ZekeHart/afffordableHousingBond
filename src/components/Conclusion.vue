e0a97e1d4c66d4dea041d9b739e7978388fc51bc
Ben Koval <bethanykoval@gmail.com>
2019-10-30 14:55:30 -0400

    Font changes

--- src/components/Conclusion.vue
+++ src/components/Conclusion.vue
@@ -1,8 +1,8 @@
 <template>
     <section id='comparison' class="conclusion">
-        <h2>NC Communities Supporting Affordable Housing</h2>
-        <p>Durham is not the only city to take a stand in support of affordable housing that can positively the community as a whole. Three NC communities have also rallied support for affordable housing.</p>
-        <h3>Charlotte</h3>
+        <h2 class='medium-title'>NC Communities Supporting Affordable Housing</h2>
+        <p class='subtitle'>Durham is not the only city to take a stand in support of affordable housing that can positively the community as a whole. Three NC communities have also rallied support for affordable housing.</p>
+        <h3 class='title'>Charlotte</h3>
         <ul>
             <li>The Charlotte region’s population has grown by 2%+ per year since 2007</li>
             <li>The number of housing units has increased at 1.3% per year during the same time</li>
@@ -10,45 +10,45 @@
             <li>Three financial institutions (Bank of America, Ally Financial, and Barings) added $70 million to the fund after voters passed the measure</li>
             <li>This shows that throwing public support behind projects can entice and incentivize private-sector companies to contribute to the cause of affordability for all</li>
         </ul>
-        <h3>Wake County</h3>
+        <h3 class='title'>Wake County</h3>
         <ul>
             <li>~100,000 county households pay more than 30% of their monthly income in rent</li>
             <li>By 2035, without intervention, the county will have a deficit of 150,000 affordable housing units</li>
             <li>In April, 2019, Wake County leaders backed nine affordable-housing developments with a $12.6 million commitment, the county’s largest financial commitment to affordable housing to-date</li>
         </ul>
-        <h3>Chapel Hill</h3>
+        <h3 class='title'>Chapel Hill</h3>
         <ul>
             <li>Over 50% of Chapel Hill’s residents pay 30% of their income or more on housing</li>
             <li>In November, 2018, voters in Chapel Hill approved a $10 million affordable housing bond that will build or maintain 700 affordable housing units</li>
         </ul>
-        <h2>
+        <h2 class='medium-title'>
             Impacts of Affordable Housing on Communities
         </h2>
         <h3></h3>
-        <p>
+        <p class='subtitle'>
             Research has shown that building and supporting affordable housing benefits entire communities, not just the direct recipients of affordable housing units.
         </p>
-        <h4>Economic Benefits</h4>
+        <h4 class='title'>Economic Benefits</h4>
         <ul>
             <li>Building 100 affordable housing units can create more than 120 jobs during construction</li>
             <li>After construction, these units can sustain 30 additional jobs</li>
             <li>Housing for employees of all pay ranges is a major factor for companies to locate in a new area and/or remain in an existing region</li>
         </ul>
-        <h4>Improved Educational Outcomes</h4>
+        <h4 class='title'>Improved Educational Outcomes</h4>
         <ul>
             <li>Building affordable housing areas has produced measurable, positive impacts on educational outcomes for low-income students</li>
         </ul>
-        <h4>Reduced Civic Administrative Costs</h4>
+        <h4 class='title'>Reduced Civic Administrative Costs</h4>
         <ul>
             <li>Reducing foreclosures and residential displacement saves governments and individual taxpayers money by reducing property maintenance costs, court and legal expenses, and social service provision</li>
         </ul>
-        <h4>Impacts on Property Values</h4>
+        <h4 class='title'>Impacts on Property Values</h4>
         <ul>
             <li>Rehabilitated existing affordable housing has been found to generally provide beneficial outcomes for neighboring property values</li>
             <li>New affordable housing has minimal impact on housing values when it is integrated into existing neighborhoods</li>
         </ul>
-        <h2 id='conclusion'>Conclusion</h2>
-        <p>
+        <h2 class='medium-title' id='conclusion'>Conclusion</h2>
+        <p class='subtitle'>
             Vote on November 5 and support Durham by supporting affordable housing. Find your polling place <a href="https://vt.ncsbe.gov/RegLkup/" class="here" target=_blank rel=_noopener >here.</a>
         <br/>
             We stand at a crossroads of two futures: we can continue on the path we are on, letting market forces dictate the future of our city, or we can take a stand, embrace the diversity of Durham, and ensure a more just and economically vibrant future for everyone, regardless of how much you can pay in rent.
