690f8ac75a5b068d75844f31e6a5bdc0e193c9f6
Zeke Hart <heirloch@gmail.com>
2019-10-30 16:11:27 -0400

    some changes to intro text and styling

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,23 +1,31 @@
 <template>
+<div class="try">
     <section class="intro">
-        <h1 class='big-title'>Durham Housing Bond Background</h1>
-        <h2 class='title'>Introduction</h2>
-        <p class='subtitle'>If you’ve lived in Durham long enough, you’ve felt and seen the changes that come from rapidly rising rents: communities in flux, neighborhoods in transition.</p>
-        
-        <p class='subtitle'>On November 5, you have the opportunity to take a stand and support Durham residents across the income spectrum.</p>
+        <div class="introDiv">
+            <h1 class='big-title'>On November 5, you have the opportunity to take a stand and support Durham residents across the income spectrum.</h1>
+        </div>
+        <div class="introDiv">
+            <div class="joey">
+                <p class='subtitle'>If you’ve lived in Durham long enough, you’ve felt and seen the changes that come from rapidly rising rents: communities in flux, neighborhoods in transition.</p>
 
-        <p class='subtitle'>There are many factors that contribute to the crisis in housing affordability and we merely scratch the surface in this website. Scroll down to learn more about why we should invest in our community through affordable housing.</p>
-        <p class='subtitle'>To address financial and demographic shifts in the city that have resulted in the affordable housing crisis, Mayor Steve Schewel proposed a $95 million affordable housing bond, described in detail <a class='here' target=_blank rel=_noopener href='https://www.durhamaffordablehousingbond.com/'>here.</a></p>
+                <p class='subtitle'>To address financial and demographic shifts in the city that have resulted in the affordable housing crisis, Mayor Steve Schewel proposed a $95 million affordable housing bond, described in detail <a class='here' target=_blank rel=_noopener href='https://www.durhamaffordablehousingbond.com/'>here.</a></p>
 
-        <p class='subtitle'>But numbers alone don’t tell the full story - our site provides the context for why passing this bond is crucial to a thriving Durham.</p>
+                <p class='subtitle'>But numbers alone don’t tell the full story - our site provides the context for why passing this bond is crucial to a thriving Durham. Scroll down to learn more about why we should invest in our community through affordable housing.</p>
+            </div>
+        </div>
     </section>
+    </div>
 </template>
 
 <style>
-
+.try {
+    height: 85vh;
+    background: linear-gradient(270deg, rgba(255,255,255,1) 30%, rgba(57,154,202,1) 100%);
+}
 .intro {
     margin-left: 10%;
     margin-right: 10%;
+    display: flex;
 }
 
 .here {
